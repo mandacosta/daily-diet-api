b2c4d49c60cf97abc85766d513fde78689153aa5
Amanda <mandacosta94@gmail.com>
2024-01-13 17:56:13 -0300

    fix: removes rollback on afterAll on setUp test file

--- test/setUp.ts
+++ test/setUp.ts
@@ -1,11 +1,7 @@
-import { afterAll, beforeAll } from 'vitest'
+import { beforeAll } from 'vitest'
 import { execSync } from 'node:child_process'
 
 beforeAll(async () => {
   execSync('npm run knex -- migrate:rollback --all') // Destrói as tabelas
   execSync('npm run knex -- migrate:latest') // Cria as tabelas
-})
-
-afterAll(async () => {
-  execSync('npm run knex -- migrate:rollback --all') // Destrói as tabelas
 })
